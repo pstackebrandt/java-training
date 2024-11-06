eabb6b8c6b94af03692b88898cc2779bf4b54d47
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-11-06 21:55:28 +0100

    init, Add .gitignore and BinaryAndHexaNum.java

---
+++ variables/BinaryAndHexaNum.java
@@ -0,0 +1,16 @@
+// BinaryAndHexaNum.java 
+
+/* tasks:
+	- compile and run file from console
+	- save hex number AA00 and binary number 10101111 to int vars
+	
+*/
+
+// javac Main.class
+// java Main
+
+public class BinaryAndHexaNum{
+	public static void main(String[] args){
+		System.out.println("binaryAndHexaNum.java started");
+	}
+}
