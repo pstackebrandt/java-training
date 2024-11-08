547374e84f36516389f923281093d1c7e83b9449
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-11-08 22:52:05 +0100

    BinaryAndHexaNum: reformated

--- variables/BinaryAndHexaNum.java
+++ variables/BinaryAndHexaNum.java
@@ -1,9 +1,11 @@
 // BinaryAndHexaNum.java 
+// Code by Peter Stackebrandt
 
-/* tasks:
-	- compile and run file from console
+/* // Task by Kofler:Java.-2022, 3.7. W6 p. 73
 	- save hex number AA00 and binary number 10101111 to int vars
-	
+
+	// Task by PS
+	- compile and run file from console
 */
 
 // javac Main.class
@@ -12,5 +14,11 @@
 public class BinaryAndHexaNum{
 	public static void main(String[] args){
 		System.out.println("binaryAndHexaNum.java started");
+
+		int fromBin = 0b10101111;
+		int fromHex = 0xAA00;
+		
+		System.out.println("     Binary representation: " + Integer.toBinaryString(fromBin) + " Decimal representation: " + fromBin);
+		System.out.println("Hexadecimal representation: " + Integer.toHexString(fromHex) + " Decimal representation: " + fromBin);
 	}
 }
